81ac25bd85565da30325a436aaaeebb0eb1b2d73
IlyPoh <pohnv888888@gmail.com>
2022-11-12 17:24:55 +0200

    Improvements

--- scripts/addElementToArray.js
+++ scripts/addElementToArray.js
@@ -1,3 +1,5 @@
+// 1 Variant 
+
 let userArray = [];
 
 function addingElementToArray() {
@@ -15,4 +17,41 @@ function sortingElements() {
 function removingElements() {
     userArray.splice(1, 3);
     document.getElementById("currentArray").innerText = userArray.join(', ');
+}
+
+
+
+// 2 Variant 
+
+let userArrayLength = Number(prompt('Enter how long your array is'))
+
+if (!userArrayLength) {
+    alert('Your entered something wrong')
+} else if (userArrayLength <= 0){
+    alert('You entered wrong number')
+} else {
+
+    for (let i = 1; i <= userArrayLength; i++) {
+        
+        if (i === 0) {
+            let elementOfArray = prompt('Enter new element:')
+            userArray.push(elementOfArray)
+        } else {
+            let elementOfArray = prompt(`Current array is: ${userArray.join(', ')}. Enter new element:`)
+            userArray.push(elementOfArray)
+        }
+        console.log(userArray)
+    }
+
+    console.log(userArray)
+    alert(`Current array is: ${userArray.join(', ')}. Now lets sort your array`)
+    userArray.sort()
+
+    console.log(userArray)
+    alert(`Current array is: ${userArray.join(', ')}. Now lets remove 2-4 elements`)
+    userArray.splice(1, 3);
+
+    console.log(userArray)
+    alert(`Your final array looks like: ${userArray.join(', ')}`)
+
 }
